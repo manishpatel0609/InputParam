a3ecca86536330b0f22722a54c6867e577bf8ed5
mvjakhaniya <mvjakhaniya@gmail.com>
2017-05-19 18:02:56 +0530

    first commit

---
+++ lib/InputParam.php
@@ -0,0 +1,122 @@
+<?php
+
+class InputParam {
+    /*
+     * @var item String | Numeric
+     *  return item value OR Boolean
+     */
+
+    public static function getPost($item, $return = FALSE) {
+        if (isset($_GET[$item])) {
+            return trim($_GET[$item]);
+        } else if (isset($_POST[$item])) {
+            return trim($_POST[$item]);
+        }
+        return $return;
+    }
+
+    /*
+     * @var item String | Numeric
+     *  return item value OR Boolean
+     */
+
+    public static function postGet($item, $return = FALSE) {
+        if (isset($_POST[$item])) {
+            return trim($_POST[$item]);
+        } else if (isset($_GET[$item])) {
+            return trim($_GET[$item]);
+        }
+        return $return;
+    }
+
+    /*
+     * @var item String | Numeric
+     *  return item value OR Boolean
+     */
+
+    public static function onlyGet($item, $return = FALSE) {
+        if (isset($_GET[$item])) {
+            return trim($_GET[$item]);
+        }
+        return $return;
+    }
+
+    /*
+     * @var item String | Numeric
+     *  return item value OR Boolean
+     */
+
+    public static function onlyPost($item, $return = FALSE) {
+        if (isset($_POST[$item])) {
+            return trim($_POST[$item]);
+        }
+        return $return;
+    }
+
+    /*
+     * @var item String | Numeric
+      return boolean
+     */
+
+    public static function issetGetPost($item) {
+        if (isset($_GET[$item])) {
+            return true;
+        } else if (isset($_POST[$item])) {
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     * @var item String | Numeric
+      return boolean
+     */
+
+    public static function issetPostGet($item) {
+        if (isset($_POST[$item])) {
+            return true;
+        } else if (isset($_GET[$item])) {
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     * @var item String | Numeric
+      return boolean
+     */
+
+    static function issetParam($item) {
+        if (isset($_POST[$item])) {
+            return true;
+        } else if (isset($_GET[$item])) {
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     *  return Array 
+    */
+    
+    static function allGetParamInArray() {
+        $arr = "";
+        foreach ($_GET as $key => $value) {
+            $arr[$key] = $value;
+        }
+        return $arr;
+    }
+
+    /*
+     * return Array
+     */
+
+    static function allPostParamInArray() {
+        $arr = "";
+        foreach ($_POST as $key => $value) {
+            $arr[$key] = $value;
+        }
+        return $arr;
+    }
+
+}
